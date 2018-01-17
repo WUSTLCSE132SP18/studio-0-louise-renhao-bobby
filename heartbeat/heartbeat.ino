d8ee163d56ca00c9784debe7e15e4dfc6de9df5b
陈嘉智 <@DESKTOP-OHA2DB5.dhcp.wustl.edu>
2018-01-17 15:48:55 -0600

    commit message

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,14 @@
+void setup (){
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop (){
+  delay(1000);
+  int time = millis();
+  digitalWrite(13,HIGH);
+  Serial.print(time/1000);
+  Serial.println(" sec have elasped");
+  digitalWrite(13,LOW);
+}
+
